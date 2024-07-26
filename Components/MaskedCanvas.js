6013dc070763f2fc52c270b911e3dec107e3a071
Jason Hui <jason903813@gmail.com>
2024-07-26 22:12:37 +0100

    add change brush size functionz

--- Components/MaskedCanvas.js
+++ Components/MaskedCanvas.js
@@ -1,8 +1,10 @@
+import { Calistoga } from 'next/font/google';
 import React, { useEffect, useRef, useState } from 'react';
 
-const MaskedCanvas = ({ canvasRef, image, reset, brushSize = 100, maskMode = true }) => {
+const MaskedCanvas = ({ canvasRef, image, reset, maskMode = true }) => {
     const [isDrawing, setIsDrawing] = useState(false);
     const [isErasing, setIsErasing] = useState(false);
+    const [brushSize, setBrushSize] = useState(20);
 
     useEffect(() => {
         if (image && canvasRef.current) {
@@ -28,6 +30,7 @@ const MaskedCanvas = ({ canvasRef, image, reset, brushSize = 100, maskMode = tru
         setIsDrawing(false);
         setIsErasing(false);
     };
+
     const draw = (e) => {
         if ((!isDrawing && !isErasing) || !maskMode) return;
 
@@ -39,21 +42,50 @@ const MaskedCanvas = ({ canvasRef, image, reset, brushSize = 100, maskMode = tru
 
         ctx.globalCompositeOperation = isErasing ? 'source-over' : 'destination-out';
         ctx.beginPath();
-        ctx.arc(x, y, 50, 0, Math.PI * 2);
+        ctx.arc(x, y, brushSize / 2, 0, Math.PI * 2);
         ctx.fill();
     };
 
+    const handleWheel = (e) => {
+        e.preventDefault()
+        const speed = 1 / 10
+        setBrushSize(prevSize => Math.floor(Math.max(10, Math.min(200, prevSize - e.deltaY * speed))));
+    };
+
+    // Need to set to passive to false to prevent scrolling
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        canvas.addEventListener('wheel', handleWheel, { passive: false });
+        return () => {
+            canvas.removeEventListener('wheel', handleWheel);
+        };
+    }, []);
+
     return (
-        <canvas
-            ref={canvasRef}
-            className="max-w-full h-auto cursor-crosshair absolute top-0 left-0"
-            onMouseDown={startDrawing}
-            onMouseUp={stopDrawing}
-            onMouseOut={stopDrawing}
-            onMouseMove={draw}
-            onContextMenu={(e) => e.preventDefault()}
-            style={{ opacity: maskMode ? 0.5 : 0 }}
-        />
+        <>
+
+            <canvas
+                ref={canvasRef}
+                className="max-w-full h-auto cursor-crosshair absolute top-0 left-0"
+                onMouseDown={startDrawing}
+                onMouseUp={stopDrawing}
+                onMouseOut={stopDrawing}
+                onMouseMove={draw}
+                onContextMenu={(e) => e.preventDefault()}
+                style={{ opacity: maskMode ? 0.5 : 0 }}
+            />
+            {maskMode && <div className=" bg-white p-4 rounded shadow w-96">
+                <input
+                    type="range"
+                    min="10"
+                    max="200"
+                    value={brushSize}
+                    onChange={(e) => setBrushSize(Number(e.target.value))}
+                    className="w-full"
+                />
+                <div className="text-center mt-2">Brush Size: {brushSize}px</div>
+            </div>}
+        </>
     );
 };
 
