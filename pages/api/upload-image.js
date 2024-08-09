fff9a07cddd5c33110628424efec10491d2eefb4
Jason Hui <jason903813@gmail.com>
2024-08-09 14:58:16 +0100

    debug 405

--- pages/api/upload-image.js
+++ pages/api/upload-image.js
@@ -9,6 +9,7 @@ export const config = {
 }
 
 export default async function handler(req, res) {
+    console.log('req.method :>> ', req.method, req);
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
