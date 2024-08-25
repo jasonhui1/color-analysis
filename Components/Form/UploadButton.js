0e7a54743d82ef88e3974605e2d37b82acbe721d
Jason Hui <jason903813@gmail.com>
2024-08-25 09:26:18 +0100

    show upload successful text

--- Components/Form/UploadButton.js
+++ Components/Form/UploadButton.js
@@ -1,46 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { uploadImageClient } from "../../lib/clientApis/image";
 import { uploadPaletteClient } from "../../lib/clientApis/palette";
-import { getUserId } from "../../lib/clientApis/supabaseClient";
 import { invertImageAlpha, processCanvas, } from "../../utils/canvas";
 
 export function UploadButton({ colorPalette, canvas, image, tags, percentage, ignorePalette, imageSourceURL, maskCanvas, invertMask = false }) {
     const [isUploading, setIsUploading] = useState(false);
+    const [uploadSuccess, setUploadSuccess] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setUploadSuccess(false);
+    }, [colorPalette, image, tags, percentage, ignorePalette, imageSourceURL, maskCanvas]);
 
     const handleUpload = async (event) => {
         if (!image) throw Error('No image uploaded');
         if (!colorPalette) throw Error('No colorPalette uploaded');
 
         setIsUploading(true);
+        setUploadSuccess(false);
+        setError(null);
+
         const processedImageURL = processCanvas({ canvas, image, useCurrentCanvas: false, cropTransparent: false });
 
-        let maskCanvas_ = maskCanvas;
-        if (invertMask) maskCanvas_ = await invertImageAlpha(maskCanvas, true);
-        const processedMaskedImageURL = processCanvas({ canvas: maskCanvas_, image, useCurrentCanvas: true, cropTransparent: false });
-
-        //TODO: Wrap this in context  when refactor
-        const id = await getUserId()
+        if (invertMask) maskCanvas = await invertImageAlpha(maskCanvas, true);
+        const processedMaskedImageURL = processCanvas({ canvas: maskCanvas, image, useCurrentCanvas: true, cropTransparent: false });
 
         try {
             const uploadedImageURL = await uploadImageClient(processedImageURL);
             const uploadedMaskedImageURL = await uploadImageClient(processedMaskedImageURL);
-            await uploadPaletteClient({ palette: { palette: colorPalette, percentage, ignorePalette }, userId: id, imageURL: uploadedImageURL, tags, imageSourceURL, maskImageURL: uploadedMaskedImageURL });
-            console.log('upload success :>> ');
-
+            await uploadPaletteClient({ palette: { palette: colorPalette, percentage, ignorePalette }, imageURL: uploadedImageURL, tags, imageSourceURL, maskImageURL: uploadedMaskedImageURL });
+            setUploadSuccess(true);
         } catch (err) {
-            console.error('Error:', err);
-            // setError('Failed to upload image. Please try again.');
             console.log('Failed to upload image. Please try again.');
+            console.error('Error:', err);
+            setError(err);
         } finally {
             setIsUploading(false);
         }
     };
+
     return (
-        <button onClick={handleUpload}
-            // disabled={isUploading} 
-            className="w-fit bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            {isUploading ? 'Uploading...' : 'Upload '}
-        </button>
+        <div className="flex flex-col">
+            <button onClick={handleUpload}
+                // disabled={isUploading} 
+                className="w-fit bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                {isUploading ? 'Uploading...' : 'Upload '}
+            </button>
+            {uploadSuccess && (
+                <p className="mt-2 text-green-600 font-semibold">Upload successful!</p>
+            )}
+        </div>
     );
 }
